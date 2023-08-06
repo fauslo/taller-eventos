eac3b7f2b837094d9cf198701f88ec8798a14a1e
Fauslo <faustino.jlc@gmail.com>
2023-08-06 01:19:37 -0300

    Agrego archivo JS

---
+++ eventos.js
@@ -0,0 +1,8 @@
+function saludar() {
+    alert("Hola!");
+}
+
+const divSaludo = document.querySelector("div");
+divSaludo.addEventListener("click", function () {
+    alert("Hola! Soy el div");
+});
