b8cd76c3dfa349eaa998995573f65b4de4f3b756
Fauslo <faustino.jlc@gmail.com>
2023-08-06 01:52:57 -0300

    Solucion agregada

--- eventos.js
+++ eventos.js
@@ -3,6 +3,7 @@ function saludar() {
 }
 
 const divSaludo = document.querySelector("div");
-divSaludo.addEventListener("click", function () {
+divSaludo.addEventListener("click", function (event) {
+    event.stopPropagation();
     alert("Hola! Soy el div");
 });
